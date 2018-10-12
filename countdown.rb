d69cee8562187e98d36b4011acc2d3fea23af276
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-12 17:40:58 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,17 +2,9 @@
 
 def countdown(number)
   while number > 0
+    sleep 1
     puts "#{number} SECOND(S)!"
     number -= 1
   end
   return "HAPPY NEW YEAR!"
 end
-
-# def countdown(number)
-#   while number > 0
-#   sleep 1
-#     puts "#{number} SECOND(S)!"
-#     number -= 1
-#   end
-#   return "HAPPY NEW YEAR!"
-# end
